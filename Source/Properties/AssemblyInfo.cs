5b319f62bf72e80a704f05955043e812b9a9fbcb
Nattakit Hosapsin <nattakit@hosapsin.com>
2021-08-23 17:28:02 +0700

    Add AssemblyInfo for versioning

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rename Pawns")]
+[assembly: AssemblyProduct("Rename Pawns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © 2021 Nattakit Hosapsin")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
+[assembly: Guid("fdec3aa8-ced6-430d-a39b-47bd5e4bf182")]
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
